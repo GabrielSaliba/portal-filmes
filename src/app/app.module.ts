f3dbb65eca2bbebb4389416a11ffa65a9826946f
GabrielSaliba <gabriel.saliba.179@gmail.com>
2020-12-04 00:30:33 -0300

    toolbar responsive

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,36 +1,43 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
-import { PopularMoviesComponent } from './views/popular-movies/popular-movies.component';
-import { HttpClientModule } from '@angular/common/http';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {PopularMoviesComponent} from './views/popular-movies/popular-movies.component';
+import {HttpClientModule} from '@angular/common/http';
 import {MatTabsModule} from '@angular/material/tabs';
-import { LatestMoviesComponent } from './views/latest-movies/latest-movies.component';
-import { NowPlayingMoviesComponent } from './views/now-playing-movies/now-playing-movies.component';
+import {LatestMoviesComponent} from './views/latest-movies/latest-movies.component';
+import {NowPlayingMoviesComponent} from './views/now-playing-movies/now-playing-movies.component';
+import {MatCardModule} from '@angular/material/card';
+import {MatMenuModule} from '@angular/material/menu';
+import {FlexLayoutModule} from '@angular/flex-layout';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    PopularMoviesComponent,
-    LatestMoviesComponent,
-    NowPlayingMoviesComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
-    HttpClientModule,
-    MatTabsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        PopularMoviesComponent,
+        LatestMoviesComponent,
+        NowPlayingMoviesComponent
+    ],
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        MatToolbarModule,
+        MatIconModule,
+        MatButtonModule,
+        HttpClientModule,
+        MatTabsModule,
+        MatCardModule,
+        MatMenuModule,
+        FlexLayoutModule
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
